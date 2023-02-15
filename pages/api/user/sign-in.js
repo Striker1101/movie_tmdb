ff7962e007ff2b33baf7c52f535f78b57a20bfaa
striker <goodluckodemilin@gmail.com>
2023-02-15 13:43:20 +0100

    removed exprt in sign in

--- pages/api/user/sign-in.js
+++ pages/api/user/sign-in.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import User from "@/pages/user";
-export default function handler(req, res) {
+function handler(req, res) {
   const id = uuidv4();
   const { username, email, password } = req.body;
   switch (req.method) {
@@ -13,7 +13,7 @@ export default function handler(req, res) {
       // Update or create data in your database
       res.status(200).json({ id, name: name || `User ${id}` });
       break;
-    default:
+    default: 
       res.setHeader("Allow", ["GET", "PUT"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
